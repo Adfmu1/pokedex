4e0128f03742325bdea5fe63cc8f18c4afa5531c
Adfmu1 <adfmu3@gmail.com>
2025-10-22 13:39:49 +0200

    add help command and its function

--- main.go
+++ main.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 	)
 
+type cliCommand struct {
+	name			string
+	description		string
+	callback		func() error
+}
+
+var commands map[string]cliCommand
+
+func initCommands() {
+	commands = map[string]cliCommand {
+		"exit": {
+			name: 			"exit",
+			description:	"Exit the Pokedex",
+			callback:		commandExit,
+		},
+		"help": {
+			name:			"help",
+			description:	"Displays a help message",
+			callback:		commandHelp,
+		},
+	}
+}
+
 func cleanInput(text string) []string {
 	str := strings.Fields(text)
 	for i := 0; i < len(str); i++ {
@@ -16,15 +39,40 @@ func cleanInput(text string) []string {
 	return str
 }
 
+func commandExit() error {
+	defer os.Exit(0)
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Usage:\n")
+	for _, comm := range commands {
+		fmt.Println(comm.name, ":", comm.description)
+	}
+	return nil
+}
+
 func main() {
+	fmt.Println("Welcome to the Pokedex!")
+	initCommands()
 	scanner := bufio.NewScanner(os.Stdin)
+
+	commandHelp()
 
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := cleanInput(scanner.Text())
-		
-		fmt.Println("Your command was:", input[0])
+		command := input[0]
 
+		if i, ok := commands[command]; !(ok) {
+			fmt.Println("Unknown command")
+		} else {
+			err := i.callback()
+			if err != nil {
+				fmt.Println(fmt.Sprintf("Error occured while trying to call %s command: %v", command, err))
+			}
+		}
 	}
 }
