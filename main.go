c1f50ee3fb5a44166cc14382a437aefab2120b2b
Adfmu1 <adfmu3@gmail.com>
2025-10-23 13:53:40 +0200

    implement the 'map' command

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"net/http"
+	"io"
+	"encoding/json"
 	)
 
 type cliCommand struct {
 	name			string
 	description		string
-	callback		func() error
+	callback		func(conf *config) error
 }
 
 var commands map[string]cliCommand
@@ -27,6 +30,11 @@ func initCommands() {
 			description:	"Displays a help message",
 			callback:		commandHelp,
 		},
+		"map": {
+			name:			"map",
+			description:	"Displays the 20 names of location areas, call again to display more",
+			callback:		commandMap,
+		},
 	}
 }
 
@@ -35,21 +43,61 @@ func cleanInput(text string) []string {
 	for i := 0; i < len(str); i++ {
 		str[i] = strings.ToLower(str[i])
 	}
-
 	return str
 }
 
-func commandExit() error {
+func commandExit(conf *config) error {
 	defer os.Exit(0)
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(conf *config) error {
 	fmt.Println("Usage:\n")
 	for _, comm := range commands {
 		fmt.Println(comm.name, ":", comm.description)
 	}
+	return nil
+}
+
+func commandMap(conf *config) error {
+	url := conf.NextUrl
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Encountered an error while trying to get url:", err)
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if res.StatusCode > 299 {
+		fmt.Println("Bad status code:", res.StatusCode)
+		return err
+	}
+	if err != nil {
+		fmt.Println("Encountered an error while retrieving body:", err)
+		return err
+	}
+
+	mLocation := LocationAreas{}
+	err = json.Unmarshal(body, &mLocation)
+	if err != nil {
+		fmt.Println("Encountered an error while unmarshalling the data:", err)
+		return err
+	}
+	fmt.Println(mLocation)
+
+	areas := mLocation.Results
+	for i := 0; i < len(areas); i++ {
+		fmt.Println(areas[i].Name)
+	}
+
+	if mLocation.NextUrl == nil {
+		conf.NextUrl = "https://pokeapi.co/api/v2/location-area/?offset=20&limit=20"
+	} else {
+		conf.NextUrl = mLocation.NextUrl
+	}
+	conf.PreviousUrl = *mLocation.PreviousUrl
 	return nil
 }
 
@@ -58,7 +106,11 @@ func main() {
 	initCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 
-	commandHelp()
+	var conf config
+	conf.NextUrl= "https://pokeapi.co/api/v2/location-area/?offset=20&limit=20"
+
+
+	commandHelp(&conf)
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -69,7 +121,7 @@ func main() {
 		if i, ok := commands[command]; !(ok) {
 			fmt.Println("Unknown command")
 		} else {
-			err := i.callback()
+			err := i.callback(&conf)
 			if err != nil {
 				fmt.Println(fmt.Sprintf("Error occured while trying to call %s command: %v", command, err))
 			}
