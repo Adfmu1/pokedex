f6bd3e8e59f465771082fd593265c1af2fdb3435
Adfmu1 <adfmu3@gmail.com>
2025-11-06 12:14:03 +0100

    add tests for Add and Get methods

---
+++ internal/pokecache/cache_test.go
@@ -0,0 +1,44 @@
+package pokecache
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestAddGet(t *testing.T) {
+	const internal = 5 * time.Second
+	cases := []struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "google.com",
+			val: []byte("google_data"),
+		},
+		{
+			key: "amazon.com",
+			val: []byte("amazon_data"),
+		},
+		{
+			key: "badsite.com",
+			val: []byte("badsite_data"),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+			cache := NewCache(int(internal))
+			cache.Add(c.key, c.val)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Error("Expected to find a key")
+				return
+			}
+			if string(val) != string(c.val) {
+				t.Error("Expected to find a value")
+				return
+			}
+		})
+	}
+}
