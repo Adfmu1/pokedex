d6cb2646f62f954849ebd43264199f70faea2615
Adfmu1 <adfmu3@gmail.com>
2025-11-12 11:25:32 +0100

    create structs for catch and inspect functions

--- structs.go
+++ structs.go
@@ -4,12 +4,40 @@ import (
 	"github.com/Adfmu1/pokedex/internal/pokecache"
 )
 
+// also serves as cache
 type config struct {
 	NextUrl     string
 	PreviousUrl string
 	Cache       *pokecache.Cache
 }
 
+// ============ POKEMONS ============
+type Pokedex struct {
+	Pokemons map[string]Pokemon
+}
+
+type Pokemon struct {
+	Name   string     `json:"name"`
+	Height int        `json:"height"`
+	Weight int        `json:"weight"`
+	Stats  []PokeStat `json:"stats"`
+	Types  []PokeType `json:"types"`
+}
+
+type PokeStat struct {
+	Stat struct {
+		Name string `json:"name"`
+	} `json:"stat"`
+	BaseValue int `json:"base_stat"`
+}
+
+type PokeType struct {
+	Type struct {
+		NamedRes string `json:"name"`
+	} `json:"type"`
+}
+
+// ============ LOCATIONS ============
 type LocationAreas struct {
 	Results     []Area  `json:"results"`
 	NextUrl     string  `json:"next"`
@@ -21,6 +49,7 @@ type Area struct {
 	Name string `json:"name"`
 }
 
+// ============ COMMANDS ============
 type cliCommand struct {
 	name        string
 	description string
